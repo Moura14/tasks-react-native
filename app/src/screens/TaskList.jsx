e755a56f3422d2ca2560224c92c04881126dd6f8
João Gabriel <87152738+Moura14@users.noreply.github.com>
2025-05-31 12:40:52 -0300

    feat: componente lista de tarefas #02

--- app/src/screens/TaskList.jsx
+++ app/src/screens/TaskList.jsx
@@ -1,12 +1,32 @@
 import { Component } from 'react'
-import { Text, View } from 'react-native'
+import { ImageBackground, StyleSheet, Text, View } from 'react-native'
+
+
+
 
 export default class TaskList extends Component{
     render(){
         return(
-            <View>
-                <Text>TeskList</Text>
+            <View style={styles.container}>
+                <ImageBackground source={require('../../../assets/imgs/today.jpg')} style={styles.background}></ImageBackground>
+                <View style={styles.taskList}>
+                    <Text>TaskList</Text>
+                </View>
             </View>
         )
     }
 }
+
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    background: {
+        flex: 3
+    },
+    taskList: {
+        flex: 7
+    }
+})
