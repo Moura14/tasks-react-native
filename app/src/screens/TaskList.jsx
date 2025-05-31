0b51cf6d6679a7f182594b86dd4df423ae2b375c
João Gabriel <87152738+Moura14@users.noreply.github.com>
2025-05-31 12:54:28 -0300

    feat: componente lista de tarefas #03

--- app/src/screens/TaskList.jsx
+++ app/src/screens/TaskList.jsx
@@ -1,14 +1,23 @@
 import { Component } from 'react'
 import { ImageBackground, StyleSheet, Text, View } from 'react-native'
 
+import moment from 'moment'
+import 'moment/locale/pt-br'
+
 
 
 
 export default class TaskList extends Component{
     render(){
+        const today = moment().locale('pt-br').format('ddd, D [de] MMMM')
         return(
             <View style={styles.container}>
-                <ImageBackground source={require('../../../assets/imgs/today.jpg')} style={styles.background}></ImageBackground>
+                <ImageBackground source={require('../../../assets/imgs/today.jpg')} style={styles.background}>
+                    <View style={styles.titleBar}>
+                        <Text>Hoje</Text>
+                        <Text>{today}</Text>
+                    </View>
+                </ImageBackground>
                 <View style={styles.taskList}>
                     <Text>TaskList</Text>
                 </View>
@@ -28,5 +37,10 @@ const styles = StyleSheet.create({
     },
     taskList: {
         flex: 7
+    },
+    titleBar: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'center'
     }
 })
