360e21aa5191b977ecc61148697d07530b99e0a1
João Gabriel <87152738+Moura14@users.noreply.github.com>
2025-06-03 12:43:28 -0300

    feat: componente tarefa #02

--- app/src/componets/Task.jsx
+++ app/src/componets/Task.jsx
@@ -1,12 +1,23 @@
-import { Text, View } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 
 
 export default props => {
     return (
-        <View>
+        <View style={styles.container}>
             <Text>{props.desc}</Text>
             <Text>{props.estimateAt + ''}</Text>
             <Text>{props.doneAt + ''}</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        borderColor: '#AAA',
+        borderBottomWidth: 1,
+        alignItems: 'center',
+        paddingVertical: 10
+
+    }
+})
