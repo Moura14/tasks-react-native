3d07135f86fdbb37bf6ab9285d4dfe84a4156bfa
João Gabriel <87152738+Moura14@users.noreply.github.com>
2025-06-24 08:58:37 -0300

    feat: modal adicionar tarefas #02

--- app/src/screens/AddTask.jsx
+++ app/src/screens/AddTask.jsx
@@ -1,5 +1,6 @@
 import { Component } from 'react'
-import { Modal, StyleSheet, TouchableWithoutFeedback, View } from 'react-native'
+import { Modal, StyleSheet, Text, TouchableWithoutFeedback, View } from 'react-native'
+import commonStyles from '../commonStyles'
 
 
 
@@ -9,6 +10,14 @@ export default class AddTask extends Component {
         return (
             <Modal transparent={true} visible={this.props.isVisible} onRequestClose={this.props.onCancel} animationType='slide'>
                 <TouchableWithoutFeedback onPress={this.props.onCancel}>
+                    <View style={styles.background}>
+
+                    </View>
+                </TouchableWithoutFeedback>
+                <View style={styles.container}>
+                    <Text style={styles.header}>Nova Tarefa</Text>
+                </View>
+                 <TouchableWithoutFeedback onPress={this.props.onCancel}>
                     <View style={styles.background}>
 
                     </View>
@@ -23,5 +32,18 @@ const styles = StyleSheet.create({
     background: {
         flex: 1,
         backgroundColor: 'rgba(0,0,0,0.7)'
+    },
+    container: {
+        flex: 1,
+        backgroundColor: '#FFF'
+    },
+    header: {
+        fontFamily: commonStyles.fontFamily,
+        backgroundColor: commonStyles.colors.today,
+        color: commonStyles.colors.secondary,
+        textAlign: 'center',
+        padding: 15,
+        fontSize: 18
+
     }
 })
